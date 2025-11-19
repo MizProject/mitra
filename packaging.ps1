85cce3883225fc673ff5f0efec05852be18dfc90
Mizumo-prjkt <bluepearl394@gmail.com>
2025-11-19 22:00:33 +0800

    feat: Remove outdated packaging scripts and update packaging.sh for deployment

--- packaging.ps1
+++
@@ -1,7 +0,0 @@
-# Mitra Deploy
-# This one is for planning to export the system away from this development folder to the
-# Real environment one
-# 
-# Though we suggest you run the setup first then deploy this
-# to get a working system
-
--- packaging.sh
+++ packaging.sh
@@ -5,3 +5,18 @@
 # Though we suggest you run the setup first then deploy this
 # to get a working system
 
+# Note: If you dont want to re-install, then this tool is fine
+
+#!/bin/bash
+
+DIRTARGET="runtime"
+DBTARGET="database.sqlite"
+
+# NODE
+NODE_LIB=("node_modules" "package.json" "package_lock.js")
+
+
+# USE ZIP AS PACKAGING
+# REMOVE THE OLD ONE IF PRESENT
+rm -rf mitra_deployment.zip
+zip -rq mitra_deployout.zip "$DIRTARGET" "$DBTARGET" "${NODE_LIB[@]}"
